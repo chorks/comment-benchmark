a5b7118b121f3a5f39347cac61b5a211cb8c98d4
Nils Knappmeier <n.knappmeier@i-views.de>
2015-05-08 12:58:13 +0200

    Doc correction (ran verb)

---
+++ README.md
@@ -0,0 +1,2 @@
+This repository contains performance test for a number of ways of extracting javascript-comments from a
+js-file. 
---
+++ index.js
@@ -0,0 +1,146 @@
+var Benchmark = require("benchmark").Benchmark;
+var suite = new Benchmark.Suite;
+
+var contents = require("fs").readFileSync("testdata.txt", "utf-8");
+var regex = /^\s*\/\*([\s\S]*?)\*\//mg;
+
+suite
+    .add("regex", function () {
+        var comment;
+        var result = [];
+        while ((comment = regex.exec(contents)) != null) {
+            result.push({
+                start: comment.index,
+                end: comment.index + comment[0].length,
+                text: comment[1]
+            });
+        }
+        return result;
+    });
+
+suite.add("iterate char-wise", function () {
+    /**
+     * Test if a string occurs at a given index within another string
+     * @param aString
+     * @param substr
+     * @param index
+     * @returns {boolean}
+     */
+    function substrTest(aString, substr, index) {
+        if (aString.length < substr.length + index) {
+            return false;
+        }
+        for (var i = 0; i < substr.length; i++) {
+            if (aString[index + i] !== substr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    var result = [];
+    var current = null;
+    for (var i = 0; i < contents.length; i++) {
+        if (current == null && substrTest(contents, "/*", i)) {
+            current = {
+                start: i
+            }
+        }
+        if (current != null && substrTest(contents, "*/", i)) {
+            current["end"] = i + 2;
+            current["text"] = contents.substr(current.start + 2, current.end - current.start - 4);
+            result.push(current);
+            current = null;
+        }
+    }
+    return result;
+});
+
+suite.add("iterate using 'indexOf'", function () {
+    var i = 0;
+    var current = null;
+    var result = [];
+    while (true) {
+        if (current == null) {
+            i = contents.indexOf("/*", i);
+            if (i == -1) {
+                break;
+            } else {
+                current = {
+                    start: i
+                }
+            }
+        } else {
+            i = contents.indexOf("*/", i);
+            if (i == -1) {
+                break;
+            } else {
+                current["end"] = i + 2;
+                current["text"] = contents.substr(current.start + 2, current.end - current.start - 4);
+                result.push(current);
+                current = null;
+            }
+        }
+    }
+    return result;
+});
+
+suite.add("iterate line-wise", function () {
+    // This snippet only finds comments that start on a new line (there may be spaces
+    // in front of the comments, but nothing else. From the dataset, if find the same
+    // number of comments as the other snippets
+    var startPattern = "/*";
+    var endPattern = "*/";
+
+    var lines = contents.split("\n");
+    var current = null;
+    var result = [];
+    for (var i = 0; i < lines.length; i++) {
+        var trimmed = lines[i].trim();
+        // Is the current line starting a comment? Check whether the line starts with a comment starter
+        if (current == null && trimmed.lastIndexOf(startPattern, 0) === 0) {
+            current = {
+                start: i,
+                lines: []
+            }
+        }
+        // This is not in an else block, because we have to check the current line as well, just to be sure
+        if (current != null) {
+            // We are within the comment. Push the current line
+            current.lines.push(trimmed);
+
+            // is thie the end of the comment?
+            var endIndex = trimmed.indexOf(endPattern);
+            if (endIndex >= 0) {
+                current.end = i + 1;
+                // Strip everything after the end of the comment (on the last comment line, including the marker)
+                var lastCommentIndex = current.lines.length - 1;
+                current.lines[lastCommentIndex] = current.lines[lastCommentIndex].substr(0, endIndex);
+                // Strip  the start-marker of the comment.
+                current.lines[0] = current.lines[0].trim().substr(startPattern.length);
+                current.text = current.lines.join("\n");
+                result.push(current);
+                current = null;
+            }
+        }
+    }
+    return result;
+});
+
+suite
+    .on('cycle', function (event) {
+        console.log(String(event.target));
+    })
+    .on('complete', function () {
+        console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+    })
+    .on('result', function(result) {
+        console.log(arguments);
+    });
+
+suite.forEach(function(test) {
+    // Output the number of comments found by each test
+    console.log(test.name,test.fn().length);
+});
+
+suite.run();
